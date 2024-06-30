0f6d1230e66cffadd716f806056c000cf34bf431
mikepreston17 <michael.n.preston@gmail.com>
2024-06-30 00:29:20 -0500

    initial

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CodeMechanic.FileSystem;
+using CodeMechanic.Types;
+using Coravel;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("Hello, World! " + DateTime.Now.ToFriendlyDateString());
+
+        // DotEnv.Load();
+        // IHost host = CreateHostBuilder(args)
+        //     .UseSystemd()
+        //     .Build();
+    }
+
+    // public static IHostBuilder CreateHostBuilder(string[] args) =>
+    //     Host.CreateDefaultBuilder(args)
+    //         .ConfigureServices(services =>
+    //         {
+    //             services.AddScheduler();
+    //             services.AddTransient<AmmoScraperInvocable>();
+    //         });
+}
+//
+// public class AmmoScraperInvocable
+// {
+// }
